811f7e1f6d798bbb64069396d1471ec72afbe766
Jan Hofmayer <hofmayer@mb101.fidor.loc>
2018-06-26 11:28:18 +0200

    sparkpost

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from sparkpost import SparkPost
+import os
 
 APP = Flask(__name__)
 
@@ -8,3 +10,18 @@ APP = Flask(__name__)
 def source():
     html = 'Hello World!'
     return html
+
+@APP.route('/send')
+def send():
+    sparky = SparkPost() # uses environment variable
+    from_email = 'test@' + os.environ.get('SPARKPOST_SANDBOX_DOMAIN') # 'test@sparkpostbox.com'
+
+    response = sparky.transmission.send(
+        use_sandbox=True,
+        recipients=['endymonium@gmail.com'],
+        html='<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>',
+        from_email=from_email,
+        subject='Oh hey!'
+    )
+
+    return response
