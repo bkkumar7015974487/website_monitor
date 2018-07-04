8d0985930947d585b13b1cddb2b7966c16baba4e
John Doe <endymonium@gmail.com>
2018-07-04 14:14:42 +0200

    moved methods into website object

--- monitor.py
+++ monitor.py
@@ -10,12 +10,11 @@ import hashlib
 from path import Path
 from bs4 import BeautifulSoup
 from aiohttp import ClientSession
-from lxml.html.diff import htmldiff
 
 import helper
 import conf
 import notifier
-from website import Website
+from website import Website, CheckFile, DiffFile
 
 """ async and aiohttp based on https://gist.github.com/dmahugh/b043ecbc4c61920aa685e0febbabb959 """
 
@@ -52,138 +51,96 @@ async def fetch(website, session):
         elapsed = default_timer() - fetch.start_time[website.slug]
         helper.p(f"{response.status} {elapsed:5.2f}s {website.name:25} {website.url} {website.css_selector}")
 
-        os.makedirs(conf.DATA_DIR, exist_ok=True)
-        with Path(conf.DATA_DIR):
+        #
+        # WRITE NEW CHECK FILE
+        #
+        check_file = CheckFile(website).create(resp)
+        website.add_check_file(check_file)
+
+        if len(website.check_files) > 1:
             #
-            # WRITE NEW CHECK FILE
+            # DIFF LAST TWO CHECK FILES
             #
-            timestamp = time.time()
-            os.makedirs(website.slug, exist_ok=True)
-            check_file_name = f"{website.slug}/{timestamp}{conf.CHECK_FILE_ENDING}"
-            with open(check_file_name, 'w', encoding="utf-8") as f:
-                f.write(resp)
-            check_file = website.add_check_file(check_file_name)
+            bs_diff, diff = website.get_diff()
 
             #
-            # DIFF ALL CHECK FILES
+            # CHECK RULES AND DECIDE IF TO CREATE A DIFF
             #
-            with Path(website.slug):
-                content_sorted = sorted(glob.glob(f"*{conf.CHECK_FILE_ENDING}"))
+            do_diff = []
+            do_diff_threshold = []
+            ins_del_tags = ('').join([el.get_text() for el in bs_diff.find_all(['ins', 'del'])])
+            if len(ins_del_tags) > 0:
+                # per default, we always create diff if there was any change at all
+                do_diff = 'True'
+                # now we check the configures threshold
+                for tag in ['ins', 'del']:
+                    for typee in ['numbers', 'letters']:
+                        s = ('').join([el.get_text() for el in bs_diff.find_all(tag)])
+                        count = 0
+                        if typee == 'numbers':
+                            count = sum(c.isdigit() for c in s)
+                        elif typee == 'letters':
+                            count = sum(c.isalpha() for c in s)
+                        helper.p(f"--> {tag} {typee} count is {count}")
+                        if count > 0:
+                            # only actually do anything if for this tag/type was a change
+                            threshold = website.get_threshold(tag, typee)
+                            helper.p(f"--> {tag} {typee} threshold is {threshold}")
+                            if threshold == 0:
+                                # 0 is the default value if there was nothing configured
+                                do_diff_threshold.append('True')
+                                helper.p(f"--> {tag} {typee} TRUE because threshold == 0")
+                            elif threshold < 0:
+                                # a negative value means, that a change of this type is ignored completely
+                                do_diff_threshold.append('False')
+                                helper.p(f"--> {tag} {typee} FALSE because threshold < 0")
+                            else:
+                                # a positive value, let's check it agains the actual count
+                                if count < threshold:
+                                    # there was a change, but the amount is too low
+                                    do_diff_threshold.append('False')
+                                    helper.p(f"--> {tag} {typee} FALSE because count ({count}) < threshold ({threshold})")
+                                else:
+                                    do_diff_threshold.append('True')
+                                    helper.p(f"--> {tag} {typee} TRUE because count ({count}) > threshold ({threshold})")
+                do_diff_threshold = "(" + " or ".join(do_diff_threshold) + ")" # merge by OR, because one TRUE is enough to create a diff
 
-                if len(content_sorted) > 1:
-                    # this means we have save an old version, compare it to the most recent one
+                do_diff = " and ".join([do_diff, do_diff_threshold]) # merge by AND, because a FALSE in the detailed check overwrites the genreal TRUE
+                helper.p(f"--> Summary: {do_diff} evals to {eval(do_diff)}")
 
-                    # first check if there was a change at all by comparing cecksums
-                    hashes = []
-                    for content_file in [ content_sorted[-2], content_sorted[-1] ]:
-                        soup = BeautifulSoup(open(content_file, encoding="utf-8"), 'lxml')
-                        for script in soup(["script", "style", "ins", "del"]):
-                            # strip out some tags
-                            script.decompose()
+            if do_diff and eval(do_diff):
+                #
+                # WRITE DIFF FILE
+                #
+                content = """
+                <style>
+                    ins {
+                        text-decoration: none;
+                        background-color: #d4fcbc;
+                        color: black;
+                        font-size: 22;
+                    }
 
-                        if website.css_selector:
-                            cont = soup.select(website.css_selector)
-                            if len(cont) > 1:
-                                sys.exit('!! selector not unique')
-                            if not cont:
-                                sys.exit(f"!! selector '{website.css_selector}' no results")
-                            cont = cont[0]
-                        else:
-                            cont = soup.html()
-
-                        hashes.append({
-                            'file_name': content_file.rstrip(conf.CHECK_FILE_ENDING),
-                            'cont': str(cont)
-                            })
-
-                    # hashes[0] = old version
-                    # hashes[1] = new version
-
-                    diff = htmldiff(hashes[0]['cont'], hashes[1]['cont'])
-                    bs_diff = BeautifulSoup(diff, 'lxml')
-                    helper.p(f"Check diff between {hashes[0]['file_name']} and {hashes[1]['file_name']}:")
-
-                    #
-                    # CHECK RULES AND DECIDE IF TO CREATE A DIFF
-                    #
-                    do_diff = []
-                    do_diff_threshold = []
-                    ins_del_tags = ('').join([el.get_text() for el in bs_diff.find_all(['ins', 'del'])])
-                    if len(ins_del_tags) > 0:
-                        # per default, we always create diff if there was any change at all
-                        do_diff = 'True'
-                        # now we check the configures threshold
-                        for tag in ['ins', 'del']:
-                            for typee in ['numbers', 'letters']:
-                                s = ('').join([el.get_text() for el in bs_diff.find_all(tag)])
-                                count = 0
-                                if typee == 'numbers':
-                                    count = sum(c.isdigit() for c in s)
-                                elif typee == 'letters':
-                                    count = sum(c.isalpha() for c in s)
-                                helper.p(f"--> {tag} {typee} count is {count}")
-                                if count > 0:
-                                    # only actually do anything if for this tag/type was a change
-                                    threshold = website.get_threshold(tag, typee)
-                                    helper.p(f"--> {tag} {typee} threshold is {threshold}")
-                                    if threshold == 0:
-                                        # 0 is the default value if there was nothing configured
-                                        #do_diff.extend(['True', 'or'])
-                                        do_diff_threshold.append('True')
-                                        helper.p(f"--> {tag} {typee} TRUE because threshold == 0")
-                                    elif threshold < 0:
-                                        # a negative value means, that a change of this type is ignored completely
-                                        do_diff_threshold.append('False')
-                                        helper.p(f"--> {tag} {typee} FALSE because threshold < 0")
-                                    else:
-                                        # a positive value, let's check it agains the actual count
-                                        if count < threshold:
-                                            # there was a change, but the amount is too low
-                                            do_diff_threshold.append('False')
-                                            helper.p(f"--> {tag} {typee} FALSE because count ({count}) < threshold ({threshold})")
-                                        else:
-                                            do_diff_threshold.append('True')
-                                            helper.p(f"--> {tag} {typee} TRUE because count ({count}) > threshold ({threshold})")
-                        do_diff_threshold = "(" + " or ".join(do_diff_threshold) + ")" # merge by OR, because one TRUE is enough to create a diff
-
-                        do_diff = " and ".join([do_diff, do_diff_threshold]) # merge by AND, because a FALSE in the detailed check overwrites the genreal TRUE
-                        helper.p(f"--> Summary: {do_diff} evals to {eval(do_diff)}")
-
-                    if do_diff and eval(do_diff):
-                        #
-                        # WRITE DIFF FILE
-                        #
-                        diff_file_name = f"{hashes[1]['file_name']}{conf.DIFF_FILE_ENDING}"
-                        helper.p(f"writing diff to {diff_file_name}")
-                        with open(diff_file_name, 'w', encoding="utf-8") as f:
-                            f.write("""
-                            <style>
-                                ins {
-                                    text-decoration: none;
-                                    background-color: #d4fcbc;
-                                    color: black;
-                                    font-size: 22;
-                                }
-
-                                del {
-                                    text-decoration: line-through;
-                                    background-color: #fbb6c2;
-                                    color: #555;
-                                    font-size: 18;
-                                }
-                                body {
-                                    color: lightgray;
-                                }
-                                a {
-                                    color: lightgray;
-                                }
-                            </style>
-                            """)
-                            f.write(diff)
-                        diff_file = check_file.add_diff_file(diff_file_name)
-                        website.notify(html=f"<a href={diff_file.url}>diff</a>")
-                    else:
-                        helper.p("no change detected")
+                    del {
+                        text-decoration: line-through;
+                        background-color: #fbb6c2;
+                        color: #555;
+                        font-size: 18;
+                    }
+                    body {
+                        color: lightgray;
+                    }
+                    a {
+                        color: lightgray;
+                    }
+                </style>
+                """
+                content += diff
+                diff_file = DiffFile(website).create(content)
+                check_file.add_diff_file(diff_file)
+                website.notify(html=f"<a href={diff_file.url}>diff</a>")
+            else:
+                helper.p("no change detected")
         return resp
 
 if __name__ == '__main__':
--- website.py
+++ website.py
@@ -3,9 +3,11 @@ import glob
 import sys
 from email.mime.text import MIMEText
 import smtplib
+import time
 
 from path import Path
-from flask import url_for
+from bs4 import BeautifulSoup
+from lxml.html.diff import htmldiff
 
 import helper
 import conf
@@ -36,23 +38,24 @@ class Website():
         if os.path.isdir(self.files_dir):
             with Path(self.files_dir):
                 # check files
-                files = glob.glob(f"*{conf.CHECK_FILE_ENDING}")
-                for _file in files:
+                _check_files = glob.glob(f"*{conf.CHECK_FILE_ENDING}")
+                for check_file in _check_files:
                     # collect diff files
                     diff_file = None
-                    timestamp = _file.rstrip(conf.CHECK_FILE_ENDING)
+                    timestamp = check_file.rstrip(conf.CHECK_FILE_ENDING)
                     diff_files = glob.glob(f"*{timestamp}*{conf.DIFF_FILE_ENDING}")
                     if len(diff_files) > 1:
-                        helper.e(f"More than one diff files ({diff_files}) found for check_file ({_file})")
+                        helper.e(f"More than one diff files ({diff_files}) found for check_file ({check_file})")
                     elif len(diff_files) == 1:
                         # create CheckFile
                         diff_file = diff_files[0]
-                    self.check_files.append(CheckFile(self, _file, diff_file))
+                    self.check_files.append(CheckFile(self, check_file, diff_file))
 
-    def add_check_file(self, check_file_name):
-        check_file = CheckFile(self, check_file_name)
-        self.check_files.append(check_file)
-        return check_file
+    def add_check_file(self, check_file):
+        if isinstance(check_file, CheckFile):
+            self.check_files.append(check_file)
+        else:
+            raise ValueError('Must be of type CheckFile')
 
     @property
     def last_change(self):
@@ -88,49 +91,94 @@ class Website():
             websites.append(Website(website_name=url_name))
         return websites
 
+    def get_diff(self):
+        """Get diff between the latest to website.check_files"""
+        hashes = []
+        for _check_file in [ self.check_files[-2], self.check_files[-1] ]:
+            soup = _check_file.soup
+            for script in soup(["script", "style", "ins", "del"]):
+                script.decompose() # strip out some tags
+
+            if self.css_selector:
+                cont = soup.select(self.css_selector)
+                if len(cont) > 1:
+                    sys.exit('!! selector not unique')
+                if not cont:
+                    sys.exit(f"!! selector '{self.css_selector}' no results")
+                cont = cont[0]
+            else:
+                cont = soup.html()
+
+            hashes.append(str(cont))
+
+        diff = htmldiff(hashes[0], hashes[1])
+        return BeautifulSoup(diff, 'lxml'), diff
+
 
 class File():
-    def __init__(self, website, file_name):
+    def __init__(self, website, file_name=None):
+        """If file_name is given, load"""
         self.website = website
-        self.file_name = file_name
-        self.file_ending = None
+        self.path = os.path.join(conf.DATA_DIR, website.slug, file_name) if file_name else None
+
+        # abstract attribute
+        self.ending = None
 
     @property
-    def file_path(self):
-        return os.path.join(self.website.files_dir, self.file_name)
+    def name(self):
+        return os.path.basename(self.path)
+
+    @property
+    def url(self):
+        return f"http://{helper.get_hostname()}/url/{self.website.slug}/diff/{self.name}"
 
     @property
     def creation_date(self):
-        timestamp = self.file_name.rstrip(self.file_ending)
+        timestamp = self.name.rstrip(self.ending)
         return helper.timestamp_to_human(timestamp)
+
+    def create(self, content):
+        """Create new"""
+        if not self.ending:
+            raise ValueError("file_ending is None")
+        if self.path:
+            raise ValueError("path alread set")
+
+        timestamp = time.time()
+        os.makedirs(self.website.files_dir, exist_ok=True)
+        self.path = os.path.join(self.website.files_dir, f"{timestamp}{self.ending}")
+        with open(self.path, 'w', encoding="utf-8") as f:
+            f.write(content)
+
+        helper.p(f"Successfully written {self.path}")
+        return self
+
+    @property
+    def soup(self):
+        return BeautifulSoup(open(self.path, encoding="utf-8"), 'lxml')
 
 
 class CheckFile(File):
-
-    def __init__(self, website, file_name, diff_file=None):
-        super().__init__(website, file_name)
+    def __init__(self, website, check_file=None, diff_file=None):
+        super().__init__(website, check_file)
         self.diff_file = DiffFile(website, diff_file) if diff_file else None
-        self.file_ending = conf.CHECK_FILE_ENDING
+        self.ending = conf.CHECK_FILE_ENDING
 
     @property
     def has_diff_file(self):
         return self.diff_file != None
 
-    def add_diff_file(self, diff_file_name):
-        diff_file = DiffFile(self.website, diff_file_name)
-        self.diff_file = diff_file
-        return diff_file
+    def add_diff_file(self, diff_file):
+        if isinstance(diff_file, DiffFile):
+            self.diff_file = diff_file
+        else:
+            raise ValueError("diff_file must of be of type DiffFile")
 
 
 class DiffFile(File):
-
-    def __init__(self, website, file_name):
-        super().__init__(website, file_name)
-        self.file_ending = conf.DIFF_FILE_ENDING
-
-    @property
-    def url(self):
-        return f"http://{helper.get_hostname()}/url/{self.website.slug}/diff/{self.file_name}"
+    def __init__(self, website, diff_file=None):
+        super().__init__(website, diff_file)
+        self.ending = conf.DIFF_FILE_ENDING
 
     @property
     def href(self):
